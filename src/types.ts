7913f0953320b4dfc04e826669165ea27f4101fc
Whitney Hahn <whitneyhahn@briefcatch.com>
2026-02-15 15:51:25 -0800

    improve types

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,10 @@
-export enum TaskStatus {
-  TODO = "to_do",
-  IN_PROGRESS = "in_progress",
-  DONE = "done",
-}
+export const TaskStatus = {
+  TODO: "to_do",
+  IN_PROGRESS: "in_progress",
+  DONE: "done",
+} as const;
+
+export type TaskStatus = (typeof TaskStatus)[keyof typeof TaskStatus];
 
 export interface Task {
   id: string;
